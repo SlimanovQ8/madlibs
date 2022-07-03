b42295610c13030a3c90ac4ac305f64444f9de99
SlimanovQ8 <Slimanovq8@gmail.com>
2022-07-03 22:44:13 +0300

    solved

--- madlibs.py
+++ madlibs.py
@@ -1,7 +1,16 @@
 def main():
 	# write your code here
-
-
+	print("Mad libs where libs get mad. \n Start below: ")
+	time = input("Enter a number from 1 to 12: ")
+	items = input("Enter  a noun  (plural): ")
+	name = input("Enter a name: ")
+	scream = input("Enter any sentence: ")
+	verb = input("Enter a verb: ")
+	print(f"It was, {time} o'clock when I heard knock a door. ")
+	print(f"I opened the door and there was a box full of, {items} with a note saying \"From Mr. {name.title()}\".")
+	print(f"Just as I closed the door I heard a scream \"{scream.upper()}\".")
+	print(f"I froze in place and all I {verb}.")
+	
 
 if __name__ == '__main__':
 	main()
